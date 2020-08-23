51766a8f7a066ec72d0a4bae2db0f102234a7271
Alexandre Moraes Matos <alexandremt03@gmail.com>
2020-08-23 01:01:04 -0300

    refactor (index): PascalCase for export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as subtitle } from '@subtitling/subtitle'
-export { default as subrip } from '@subtitling/format/subrip/subrip.format'
-export { default as subripCue } from '@subtitling/format/subrip/cue/subrip-cue.format'
+export { default as Subtitle } from '@subtitling/subtitle'
+export { default as Subrip } from '@subtitling/format/subrip/subrip.format'
+export { default as SubripCue } from '@subtitling/format/subrip/cue/subrip-cue.format'
